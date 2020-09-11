96d8d5059013606964685912d58b0b3a0b5dfed2
tmerlier <theophile.merliere@gmail.com>
2020-09-11 14:35:07 +0200

    Add Anomalies

--- helpers/bases-locales.js
+++ helpers/bases-locales.js
@@ -1,4 +1,5 @@
 import {getPublishedBasesLocales} from '../lib/adresse-backend'
+import {getVoies, getNumeros} from '../lib/bal-api'
 
 export const expandWithPublished = async basesLocales => {
   const publishedBasesLocales = await getPublishedBasesLocales()
@@ -7,4 +8,34 @@ export const expandWithPublished = async basesLocales => {
   })
 
   return publishedBasesLocales
+}
+
+export const checkAnomalies = async basesLocales => {
+  const anomalies = {}
+  await Promise.all(basesLocales.map(async ({_id, communes}) => {
+    const communesVoies = await Promise.all(communes.map(async codeCommune => {
+      return getVoies(_id, codeCommune)
+    }))
+
+    communesVoies.forEach(voies => {
+      voies.forEach(async voie => {
+        if (voie.nom === voie.nom.toUpperCase()) {
+          anomalies[_id] = {uppercase: true}
+        }
+
+        const numeros = await getNumeros(voie._id)
+        numeros.forEach(({numero}) => {
+          if (numero >= 5000) {
+            if (anomalies[_id]) {
+              anomalies[_id].virtual = true
+            } else {
+              anomalies[_id] = {virtual: true}
+            }
+          }
+        })
+      })
+    })
+  }))
+
+  return anomalies
 }
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,15 +1,17 @@
-import React, {useState, useMemo} from 'react'
+import React, {useState, useEffect, useMemo, useCallback} from 'react'
 import PropTypes from 'prop-types'
-import {Pane, Heading, Paragraph, Table, Badge} from 'evergreen-ui'
+import Router from 'next/router'
+import {Pane, Heading, Paragraph, Table, Tooltip, Icon, Badge, Spinner} from 'evergreen-ui'
 
 import {listBasesLocales} from '../../lib/bal-api'
-import {expandWithPublished} from '../../helpers/bases-locales'
+import {expandWithPublished, checkAnomalies} from '../../helpers/bases-locales'
 import {formatDate} from '../../helpers/date'
 
 import useFuse from '../../hooks/fuse'
 
 function Index({basesLocales}) {
-  const [selected, setSelected] = useState(null)
+  const [anomalies, setAnomalies] = useState({})
+  const [anomaliesLoading, setAnomaliesLoading] = useState(true)
 
   const [filtered, onFilter] = useFuse(basesLocales, 200, {
     keys: [
@@ -28,6 +30,42 @@ function Index({basesLocales}) {
     return <Badge color='neutral'>Brouillon</Badge>
   }, [])
 
+  const handleSelect = baseLocale => {
+    const {_id, communes} = baseLocale
+    if (communes.length === 1) {
+      Router.push(
+        `/bal/commune?balId=${_id}&codeCommune=${communes[0]}`,
+        `/bal/${_id}/communes/${communes[0]}`
+      )
+    } else {
+      Router.push(`/bal?balId=${_id}`, `/bal/${_id}`)
+    }
+  }
+
+  const getTooltip = useCallback(baseLocaleId => {
+    const arr = []
+
+    if (anomalies[baseLocaleId].uppercase) {
+      arr.push('Nom de voie en lettre capitale')
+    }
+
+    if (anomalies[baseLocaleId].virtual) {
+      arr.push('Numéro virtuel')
+    }
+
+    return arr.join(', ')
+  }, [anomalies])
+
+  useEffect(() => {
+    const getAnomalies = async basesLocales => {
+      const anomalies = await checkAnomalies(basesLocales)
+      setAnomalies(anomalies)
+      setAnomaliesLoading(false)
+    }
+
+    getAnomalies(basesLocales)
+  }, [basesLocales])
+
   return (
     <Pane backgroundColor='white' display='flex' flexDirection='column' height='100%'>
       <Pane padding={16}>
@@ -44,10 +82,11 @@ function Index({basesLocales}) {
           <Table.TextHeaderCell>Date de création</Table.TextHeaderCell>
           <Table.TextHeaderCell>Dernière mise à jour</Table.TextHeaderCell>
           <Table.TextHeaderCell>Statut</Table.TextHeaderCell>
+          <Table.TextHeaderCell>Anomalies</Table.TextHeaderCell>
         </Table.Head>
         <Table.VirtualBody>
           {filtered.length > 0 ? filtered.map(baseLocale => (
-            <Table.Row key={baseLocale._id} isSelectable onSelect={() => setSelected(baseLocale)}>
+            <Table.Row key={baseLocale._id} isSelectable onSelect={() => handleSelect(baseLocale._id)}>
               <Table.TextCell>{baseLocale.nom}</Table.TextCell>
               <Table.TextCell>{baseLocale.communes.length > 0 ? baseLocale.communes.join(', ') : '-'}</Table.TextCell>
               <Table.TextCell>{formatDate(baseLocale._created)}</Table.TextCell>
@@ -55,6 +94,16 @@ function Index({basesLocales}) {
               <Table.Cell>
                 {statusBadge}
               </Table.Cell>
+              <Table.TextCell>
+                {anomaliesLoading ? <Spinner size={16} /> :
+                  anomalies[baseLocale._id] ? (
+                    <Tooltip content={getTooltip(baseLocale._id)}>
+                      <Icon icon='warning-sign' color='warning' />
+                    </Tooltip>
+                  ) :
+                    <Icon icon='tick' color='success' />
+                }
+              </Table.TextCell>
             </Table.Row>
           )) : (
             <Table.Row>
